efad6d92873daa99b9b2294febb34ed66f6d3599
luuquangduc7cd <luuquangduc7cd@gmail.com>
2020-05-24 16:30:32 +0700

    Python code

--- HomeServer/main.py
+++ HomeServer/main.py
@@ -1,4 +1,19 @@
-print("hello main")
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
 
-if __name__ == "__main__":
-    main()
+dhtPin = 4
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(dhtPin, GPIO.IN)
+
+dhtType = Adafruit_DHT.DHT22
+
+if __name__ == "__main__": 
+    try:
+        while True: 
+            humidity, temperature = Adafruit_DHT.read_retry(dhtType, dhtPin)
+            time.sleep(1)
+    except KeyboardInterrupt:
+            print('Interruped !')
