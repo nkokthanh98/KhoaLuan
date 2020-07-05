3647347ca8fa8706a2059541925a469f65671727
nkokthanh98 <65352536+nkokthanh98@users.noreply.github.com>
2020-07-05 23:17:29 +0700

    Update user.js

--- WebServer/apps/models/user.js
+++ WebServer/apps/models/user.js
@@ -30,6 +30,52 @@ function getUser(username) {
     }
     return false;
 }
+
+function addData(data) {
+    if(data){
+        let defer = q.defer();
+        let sql = `INSERT into sensordata (did, datavalue, time) values (${data.did}, ${data.value}, CURRENT_TIMESTAMP)`
+        let query = conn.query(sql, (err, result) => {
+            if(err){
+                defer.reject(err);
+            } else {
+                defer.resolve(result);
+            }
+        });
+        return defer.promise;
+    }
+    return false;
+}
+
+function getDataByDID(did) {
+    if(did){
+        let defer = q.defer();
+        let sql = `SELECT datavalue, time FROM sensordata WHERE did = ${did} and date(time) = date(current_timestamp) and month(time) = month(current_timestamp) and year(time) = year(current_timestamp) order by time asc`
+        let query = conn.query(sql, (err, result) => {
+            if(err)
+                defer.reject(err);
+            else
+                defer.resolve(result)
+        });
+        return defer.promise;
+    }
+    return false;
+}
+
+function getAvgByDID(did) {
+    if(did){
+        let defer = q.defer();
+        let sql = `SELECT avg(datavalue) FROM sensordata WHERE did = ${did} and date(time) = date(current_timestamp) and month(time) = month(current_timestamp) and year(time) = year(current_timestamp)`
+        let query = conn.query(sql, (err, result) => {
+            if(err)
+                defer.reject(err);
+            else
+                defer.resolve(result)
+        });
+        return defer.promise;
+    }
+    return false;
+}
 module.exports = {
     addUser: addUser,
     getUser: getUser
