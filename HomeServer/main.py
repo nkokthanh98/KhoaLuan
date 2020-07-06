4088887a39ea0d28bf9964098590abb4defd0412
nkokthanh98 <65352536+nkokthanh98@users.noreply.github.com>
2020-07-06 09:12:12 +0700

    Update main.py

--- HomeServer/main.py
+++ HomeServer/main.py
@@ -1,16 +1,4 @@
-import RPi.GPIO as GPIO
-import Adafruit_DHT
-import time
+print("hello main")
 
-dhtPin = 4
-
-dhtType = Adafruit_DHT.DHT22
-
-if __name__ == "__main__": 
-    try:
-        while True: 
-            humidity, temperature = Adafruit_DHT.read_retry(dhtType, dhtPin)
-            print("Humidity: " + str(humidity) + " Temperature: " + str(temperature))
-            time.sleep(1)
-    except KeyboardInterrupt:
-            print('Interruped !')
+if __name__ == "__main__":
+    main()
